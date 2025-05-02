3404396d56d2fc0b9fd9407dfa511eac5e6d5412
Leo79Jp <leojparodi@gmail.com>
2025-05-02 19:12:34 -0300

    Complete ejerecicio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
 
 // Cambiar esta función por la lectura del archivo de frutas con fs
 function leerFrutas() { 
@@ -15,6 +16,16 @@ function leerFrutas() {
   return frutasData;
 }
 
+let frutas = [];
+fs.readFile('./frutas.json', (err, datos) =>{
+  if (err){
+    console.error('Error al leer el archivo JSON: ', err);
+  }else{
+    frutas = JSON.parse(datos)
+  }
+})
+
+
 // Crear el servidor HTTP
 const servidor = http.createServer((req, res) => {
   // Configurar el header de respuesta como JSON
@@ -24,16 +35,50 @@ const servidor = http.createServer((req, res) => {
   const path = url.parse(req.url).pathname;
   
   // TODO: Implementar el manejo de las siguientes rutas:
+
   // 1. '/' - Mensaje de bienvenida
-  // 2. '/frutas/all' - Devolver todas las frutas
-  // 3. '/frutas/id/123' - Devolver una fruta por su ID
-  // 4. '/frutas/nombre/manzana' - Buscar frutas por nombre (parcial)
-  // 5. '/frutas/existe/manzana' - Verificar si existe una fruta
-  // 6. Cualquier otra ruta - Error 404
+  if (path === '/'){
+    res.writeHead(200, {'content-type' : 'application/json'})
+    res.end('Ejercitacion con Archivo JSON de frutas!')
+
+    // 2. '/frutas/all' - Devolver todas las frutas
+  }else if (path ==='/frutas/all'){
+    res.writeHead(200, {'content-type' : 'application/json'})
+    res.end(JSON.stringify(frutas))
+
+    // 3. '/frutas/id/123' - Devolver una fruta por su ID
+  }else if (path.startsWith('/frutas/id/')){
+    const partes = path.split('/')
+    const i = partes[3]-1
+    if (i >= 0 && i < frutas.length){
+        const fruta = frutas[i]
+        res.writeHead(200, {'content-type' : 'application/json'})
+        res.end(JSON.stringify(fruta))
+    }else{
+        res.writeHead(404, {'content-type': 'application/json'})
+        res.end(JSON.stringify({Error: 'Indice de frutas esta fuera de rango🧐'}))
+    }
+
+    // 4. '/frutas/nombre/manzana' - Buscar frutas por nombre (parcial)
+}else if (path.startsWith('/frutas/nombre/')){
+  const partes = path.split('/')
+  const nombre = partes[3].toLocaleLowerCase()
+  const result = frutas.filter(fruta => 
+      fruta.nombre.toLocaleLowerCase().includes(nombre))
+    // 5. '/frutas/existe/manzana' - Verificar si existe una fruta
+      if (result){
+        res.writeHead(200, {'content-type' : 'application/json'})
+        res.end(JSON.stringify(result))
+      }
+    // 6. Cualquier otra ruta - Error 404
+}else{
+  res.writeHead(404, {'content-type': 'application/json'})
+  res.end(JSON.stringify({Error: 'Ruta no encontrada😒'}))
+}
   
   // Por ahora, devolvemos un 404 para todas las rutas
-  res.statusCode = 404;
-  res.end(JSON.stringify({ error: 'Ruta no encontrada' }));
+  // res.statusCode = 404;
+  // res.end(JSON.stringify({ error: 'Ruta no encontrada!😒' }));
 });
 
 // Iniciar el servidor
